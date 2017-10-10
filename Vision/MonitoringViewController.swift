988f315537bebdff7bc91d3f671749455d1ccc86
Gilles Dubois <gilles.dubois@factorfx.com>
2017-10-10 21:47:05 +0200

    Add already added servers informations and set properties in swift controller

--- Vision/MonitoringViewController.swift
+++ Vision/MonitoringViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class MonitoringViewController: UIViewController {
-
+    
+    // UI Properties
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var serverAddress: UITextField!
+    @IBOutlet weak var icmpSwitch: UISwitch!
+    @IBOutlet weak var httpSwitch: UISwitch!
+    @IBOutlet weak var refreshRateSlider: UISlider!
+    @IBOutlet weak var refreshLabel: UILabel!
+    
+    // Below UI properties are only shown on edit ( moniroting already set )
+    @IBOutlet weak var statusImage: UIImageView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
