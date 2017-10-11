30f4ac2c58b002405dde654dcb6888e7d989f6b4
Gilles Dubois <gilles.dubois@factorfx.com>
2017-10-11 07:50:53 +0200

    Add Monitoring Object and monitorint image status class

---
+++ Vision/MonitoringStatus.swift
@@ -0,0 +1,13 @@
+//
+//  MonitoringStatus.swift
+//  Vision
+//
+//  Created by Gilles Dubois on 11/10/2017.
+//  Copyright © 2017 Gilles Dubois. All rights reserved.
+//
+
+import UIKit
+
+class MonitoringStatus: UIImage {
+
+}
