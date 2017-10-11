32fa2c0f98a86e64ca8fe8b6cd126ab5e337b57d
Gilles Dubois <gilles.dubois@factorfx.com>
2017-10-11 19:43:14 +0200

    Add object properties and init

--- Vision/Monitoring.swift
+++ Vision/Monitoring.swift
@@ -7,9 +7,36 @@
 //
 
 import UIKit
+import os.log
 
 class Monitoring: NSObject  {
 
-
+    // MARK : Properties
+    var name: String
+    var serverUrl: String
+    var isIcmp: Bool
+    var isHttp: Bool
+    var refreshRate: Int
+    
+    //MARK: Initialization
+    init?(name: String, serverUrl: String, isIcmp: Bool, isHttp: Bool, refreshRate: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // server url too
+        guard !serverUrl.isEmpty else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.serverUrl = serverUrl
+        self.isIcmp = isIcmp
+        self.isHttp = isHttp
+        self.refreshRate = refreshRate
+    }
 
 }
