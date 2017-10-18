5310dad854c454c19683401346f7cba3e37d5dbe
Gilles Dubois <gilles.dubois@factorfx.com>
2017-10-18 21:20:48 +0200

    Add mangagement between view and segues

--- Vision/MonitoringTableViewController.swift
+++ Vision/MonitoringTableViewController.swift
@@ -7,33 +7,68 @@
 //
 
 import UIKit
+import os.log
 
 class MonitoringTableViewController: UITableViewController {
     
     //MARK: Properties
     var mons = [Monitoring]()
     
-    //MARK: Private Methods
-    private func loadSamples() {
-        
-        guard let mon1 = Monitoring(name: "Monitor 1", serverUrl: "192.168.1.100", isIcmp: true, isHttp: false, refreshRate : 40) else {
-            fatalError("Unable to instantiate mon1")
+    //MARK: Actions
+    @IBAction func unwindToMonitoringList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? MonitoringViewController, let monitoring = sourceViewController.monitoring {
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // Update an existing monitoring.
+                mons[selectedIndexPath.row] = monitoring
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }else{
+                // Add a new monitoring.
+                let newIndexPath = IndexPath(row: mons.count, section: 0)
+                
+                mons.append(monitoring)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
         }
+    }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        super.prepare(for: segue, sender: sender)
         
-        guard let mon2 = Monitoring(name: "Monitor 2", serverUrl: "192.168.1.10", isIcmp: true, isHttp: false, refreshRate : 50) else {
-            fatalError("Unable to instantiate mon2")
+        switch(segue.identifier ?? "") {
+        case "AddItem":
+            os_log("Adding a new monitoring.", log: OSLog.default, type: .debug)
+        case "EditItem":
+            guard let monitoringDetailViewController = segue.destination as? MonitoringViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            guard let selectedMonitoringCell = sender as? MonitoringTableViewCell else {
+                fatalError("Unexpected sender: \(String(describing: sender))")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedMonitoringCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            let selectedMonitoring = mons[indexPath.row]
+            monitoringDetailViewController.monitoring = selectedMonitoring
+        default:
+            fatalError("Unexpected Segue Identifier; \(String(describing: segue.identifier))")
         }
-        
-        guard let mon3 = Monitoring(name: "Monitor 3", serverUrl: "192.168.1.1", isIcmp: true, isHttp: false, refreshRate : 30) else {
-            fatalError("Unable to instantiate mon3")
-        }
-        
-        mons += [mon1, mon2, mon3]
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
         
         loadSamples()
 
@@ -71,7 +106,7 @@ class MonitoringTableViewController: UITableViewController {
             fatalError("The dequeued cell is not an instance of Monitoring Table View Cell.")
         }
         
-        // Fetches the appropriate meal for the data source layout.
+        // Fetches the appropriate monitoring for the data source layout.
         let mon = mons[indexPath.row]
         
         cell.serverUrl.text = mon.serverUrl
@@ -88,17 +123,16 @@ class MonitoringTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            mons.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
@@ -125,4 +159,23 @@ class MonitoringTableViewController: UITableViewController {
     }
     */
 
+    
+    //MARK: Private Methods
+    private func loadSamples() {
+        
+        guard let mon1 = Monitoring(name: "Monitor 1", serverUrl: "192.168.1.100", isIcmp: true, isHttp: false, refreshRate : 40) else {
+            fatalError("Unable to instantiate mon1")
+        }
+        
+        guard let mon2 = Monitoring(name: "Monitor 2", serverUrl: "192.168.1.10", isIcmp: true, isHttp: false, refreshRate : 50) else {
+            fatalError("Unable to instantiate mon2")
+        }
+        
+        guard let mon3 = Monitoring(name: "Monitor 3", serverUrl: "192.168.1.1", isIcmp: true, isHttp: false, refreshRate : 30) else {
+            fatalError("Unable to instantiate mon3")
+        }
+        
+        mons += [mon1, mon2, mon3]
+        
+    }
 }
--- Vision/MonitoringViewController.swift
+++ Vision/MonitoringViewController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
-
-/*
- This value is either passed by `MonitoringTableViewController`
- */
-var monitoring: Monitoring?
+import os.log
 
 class MonitoringViewController: UIViewController, UITextFieldDelegate{
+    
+    /*
+     This value is either passed by `MonitoringTableViewController`
+     */
+    var monitoring: Monitoring?
     
     // MARK : UI Properties
     
@@ -41,6 +42,15 @@ class MonitoringViewController: UIViewController, UITextFieldDelegate{
         // Handle the text field’s user input through delegate callbacks.
         self.serverAddress.delegate = self
         self.name.delegate = self
+        
+        // Set up views if editing an existing Monitoring.
+        if let monitoring = monitoring {
+            navigationItem.title = monitoring.name
+            name.text = monitoring.name
+            icmpSwitch.isOn = monitoring.isIcmp
+            httpSwitch.isOn = monitoring.isHttp
+            refreshRateSlider.value = Float(monitoring.refreshRate)
+        }
         
         // Disable save button by default
         saveButton.isEnabled = false
@@ -80,6 +90,54 @@ class MonitoringViewController: UIViewController, UITextFieldDelegate{
     private func setRefreshLabelDepOnSlider(){
         let currentValue = Int(self.refreshRateSlider.value)
         self.refreshLabel.text = "\(currentValue) Min(s)"
+    }
+    
+    //MARK: Navigation
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        /*
+         Var used :
+            @IBOutlet weak var name: UITextField!
+            @IBOutlet weak var serverAddress: UITextField!
+            @IBOutlet weak var icmpSwitch: UISwitch!
+            @IBOutlet weak var httpSwitch: UISwitch!
+            @IBOutlet weak var refreshRateSlider: UISlider!
+            @IBOutlet weak var refreshLabel: UILabel!
+         */
+        let name = self.name.text
+        let srvAddress = self.serverAddress.text
+        let icmpSwitch = self.icmpSwitch.isOn
+        let httpSwitch = self.httpSwitch.isOn
+        let refreshRateSlider = self.refreshRateSlider.value
+        
+        // Set the monitoring object to be passed to MonitoringTableViewController after the unwind segue.
+        monitoring = Monitoring(name: name!, serverUrl: srvAddress!, isIcmp: icmpSwitch, isHttp: httpSwitch, refreshRate : Int(refreshRateSlider))
+        
+        
+    }
+    
+    //MARK: ACTIONS
+    @IBAction func CancelButtonAction(_ sender: Any) {
+        
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMonitoringMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMonitoringMode {
+            dismiss(animated: true, completion: nil)
+        }else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }else {
+            fatalError("The MonitoringViewController is not inside a navigation controller.")
+        }
     }
 
 
